3e06757be1e66039f8c7646676fe5d3e79f0edbf
Retr0jE <isip_d.r.horkin@mpt.ru>
2022-10-17 12:49:33 +0300

    DBmOvies

---
+++ Proektik/src/main/java/ru/spring/Proektik/ProektikApplication.java
@@ -0,0 +1,13 @@
+package ru.spring.Proektik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProektikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProektikApplication.class, args);
+	}
+
+}
